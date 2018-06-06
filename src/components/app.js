b7ccc013d082e26d4afa57fd5a77b7cc597cb1c8
Helen Zhou <helen.zhou6@gmail.com>
2018-06-06 21:52:46 +0100

    Initial setup

---
+++ src/components/app.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default class App extends React.Component {
+  state = {
+    pageView: "landing",
+  };
+
+  render() {
+    return (
+      <h1>Hello World</h1>
+    );
+  }
+}
