ce7540fc1be0caad5503f8184c01b66b5d539567
Helen Zhou <helen.zhou6@gmail.com>
2018-06-08 09:09:09 +0100

    Add google geolocating API - based on input will geolocate relate #4

--- src/components/form.js
+++ src/components/form.js
@@ -1,6 +1,7 @@
 import React from "react";
 import getAPIData from "../utilities/getAPIData";
 import Results from "./results";
+import changeSpacesToPlus from "../utilities/changeSpacesToPlus";
 
 export default class Form extends React.Component {
   state = {
@@ -8,18 +9,36 @@ export default class Form extends React.Component {
     responseData: null,
     message: ""
   };
+  defaultErrorMessage = "Oops, something went wrong on our end, try again later.";
 
   getWeather = (latitude, longitude) => {
     getAPIData(`http://localhost:3001/api/darksky/${latitude}/${longitude}`)
       .then(res => res.error ? this.addMessage(res.error) : this.setState({ responseData: res.daily.data[0], message: "" }))
       .catch(err => {
         console.log(`Error with the API request to back end server - ${err.message}`);
-        this.addMessage("Oops, an error on our end, try again later");
+        this.addMessage(this.defaultErrorMessage);
       });
   }
 
   addMessage = (msg) => {
     this.setState({ message: msg, responseData: null });
+  }
+
+  onInputSubmit = () => {
+    getAPIData(`https://maps.googleapis.com/maps/api/geocode/json?address=${changeSpacesToPlus(this.state.address)}&key=${process.env.API_GOOGLE}`)
+      .then(res => {
+        if (res.error_message) {
+          console.log(`Error with the API request to Google API - ${res.error_message}`);
+          this.addMessage(this.defaultErrorMessage);
+        } else {
+          const { lat: latitude, lng: longitude } = res.results[0].geometry.location;
+          this.getWeather(latitude, longitude);
+        }
+      })
+      .catch(err => {
+        console.log(`Error with the API request to Google API - ${err.message}`);
+        this.addMessage(this.defaultErrorMessage);
+      });
   }
 
   onSubmit = () => {
@@ -39,8 +58,10 @@ export default class Form extends React.Component {
     if (!responseData) {
       return (
         <React.Fragment>
-          <input></input>
-          <button >Submit</button>
+          <label>Input address:
+          <input id="address-input" value={address} onChange={e => this.setState({ address: e.target.value })} />
+          </label>
+          <button onClick={this.onInputSubmit}>Submit</button>
           <p>Or use current location</p>
           <button onClick={this.onSubmit}>Go</button>
           <p>{message}</p>
